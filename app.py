fe23d26bca035acf9d9862c1caace4b9c831cb23
pablitus03 <91649504+pablitus03@users.noreply.github.com>
2023-10-08 21:32:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
-
-# Instala las bibliotecas necesarias
-#!pip install googletrans==4.0.0-rc1
-#!pip install gTTS
-
-# Importa las bibliotecas
+import streamlit as st
 from googletrans import Translator
 from gtts import gTTS
-from IPython.display import Audio
 
-# Solicita la frase y el idioma de destino
-frase = input("Ingresa la frase que quieres traducir: ")
-idioma_destino = input("Ingresa el idioma de destino (por ejemplo, 'es' para español): ")
-# Crea una instancia del traductor
-traductor = Translator()
-# Traduce la frase al idioma de destino
-traduccion = traductor.translate(frase, dest=idioma_destino)
-frase_traducida = traduccion.text
-# Crea una instancia de gTTS para generar el audio
-# Imprime la frase traducida
-print("Frase traducida:", frase_traducida)
-tts = gTTS(text=frase_traducida, lang=idioma_destino)
-tts.save("temp.wav")
-sound_file = "temp.wav"
-Audio(sound_file, autoplay=True)
-
+st.title("Aplicativo AMA")
+st.image("translate.png", width=200)
+st.subheader("Texto a audio y traducción.")
+text = st.text_input("Ingrese el texto:")
+languages = {"Inglés": "en", "Español": "es", "Chino Mandarín": "zh-cn", "Francés": "fr", "Ruso": "ru", "Hindi": "hi"}
+target_lang = st.selectbox("Seleccione el idioma de destino:", list(languages.keys()))
+if text and target_lang:
+    source_lang = "es"
+    target_lang_code = languages[target_lang]
+    translator = Translator()
+    translated_text = translator.translate(text, src=source_lang, dest=target_lang_code).text
+    tts = gTTS(translated_text, lang=target_lang_code, slow=False)
+    tts.save("temp/audio.mp3")
+    audio_file = open("temp/audio.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown("## Tu audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    st.markdown("## Texto en audio:")
+    st.write(translated_text)
